9a1beed91b3f5c0e8f850c00d51ceb424ad63024
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2024-01-26 21:15:11 +0300

    error on mongo string

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://OsegoTech:Mongo2000.@cluster0.dz97srz.mongodb.net/Mall-Hub?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
