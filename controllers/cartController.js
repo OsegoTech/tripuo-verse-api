c7e8a842d29995cf03d9d677a205aee6e976e06d
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2024-01-26 17:43:27 +0300

    cart to Cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,4 +1,4 @@
-import Cart from "../models/cartModel.js";
+import Cart from "../models/CartModel.js";
 import asyncHandler from "express-async-handler";
 
 export const createCart = asyncHandler(async (req, res) => {
