3ba8a949755d0fa3e31c6cd5b1affc415ed5d360
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2024-01-27 22:04:40 +0300

    removed the client dir

--- frontend/dist/assets/NotFound-3aeb0836.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,c as n,a as e}from"./index-2aef0a2d.js";const c={},s=e("h1",null,"404",-1),_=e("p",null,"Page not found",-1),a=[s,_];function r(d,l){return t(),n("div",null,a)}const f=o(c,[["render",r]]);export{f as default};
